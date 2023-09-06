a5507ca635050c084ae73c8d746bfaa00486d7fc
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-09-05 23:01:52 -0300

    Refactor - Switch it Up!

--- python/switch_it_up.py
+++ python/switch_it_up.py
@@ -7,26 +7,32 @@ import unittest
 """
 
 
-from typing import Dict
-
-switch: Dict[int, str] = {
-    0: "Zero",
-    1: "One",
-    2: "Two",
-    3: "Three",
-    4: "Four",
-    5: "Five",
-    6: "Six",
-    7: "Seven",
-    8: "Eight",
-    9: "Nine"
-}
-
-
 def switch_it_up(number: int) -> str:
-    """Adapted Switch-Case from Python"""
+    """Switch-Case in Python"""
 
-    return switch.get(number, "The number is not from 0 to 9!")
+    match number:
+        case 0:
+            return 'Zero'
+        case 1:
+            return 'One'
+        case 2:
+            return 'Two'
+        case 3:
+            return 'Three'
+        case 4:
+            return 'Four'
+        case 5:
+            return 'Five'
+        case 6:
+            return 'Six'
+        case 7:
+            return 'Seven'
+        case 8:
+            return 'Eight'
+        case 9:
+            return 'Nine'
+        case _:
+            return 'The number is not from 0 to 9!'
 
 
 """
